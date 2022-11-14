7b6b33cf2e407bf16c115148e617274a1fb2fde0
Ines Garcia Ortiz <ines.garciaortiz99@gmail.com>
2022-11-14 16:51:28 +0100

    gsea almost done

--- MyGSEA.R
+++ MyGSEA.R
@@ -20,50 +20,56 @@ suppressPackageStartupMessages({
   library(RColorBrewer, quietly = TRUE)
   library(org.Hs.eg.db)
   library(fgsea, quietly = T)
+  library(msigdbr, quietly = T)
 })
 
-data <- read.delim('MyResults_DEG/sig_pval.tsv', sep= "\t", header=T, row.names = 1)
+data <- read.delim('MyResults_DEG/all_genes.tsv', sep= "\t", header=T, row.names = 1)
 
 #Calculate GSEA
 
-dat <- data$log2FoldChange
+dat <- data$stat #define why use stat
 names(dat) <- as.character(rownames(data))
 dat <- sort(dat, decreasing=TRUE)
 
 #Calculate GSEA and write tables of results
-set.seed(1)
-egs <- gseGO(geneList = dat, OrgDb = "org.Hs.eg.db", keyType = "ENSEMBL", 
-             ont= "ALL")
-egs_genename <- setReadable(egs, OrgDb = "org.Hs.eg.db")
 
-write.table(egs, file = "MyResults_GSEA/tableGO_GSEA.txt", sep= "\t", quote = F)
-write.table(egs_genename, file = "MyResults_GSEA/tableGO_GSEA_genename.txt", sep= "\t", quote = F)
+category <- 'H'
+db_sets <- msigdbr(species = 'Homo sapiens', category = category)%>% 
+  dplyr::select(gs_name, ensembl_gene)
+head(db_sets) #each gene associated with each msig group
+
+set.seed(1)
+egs <- GSEA(geneList = dat, pvalueCutoff = 0.05, eps = 0, pAdjustMethod = "BH", 
+            seed = T, TERM2GENE = db_sets)
+head(egs@result)
+egs_df <- data.frame(egs@result)
+egs_df <- egs_df[, -c(1,2)]
+
+write.table(egs_df, file = 'MyResults_GSEA/GSEA_results.txt', sep= "\t", 
+            quote = F, row.names = T)
 
 ##Dotplot / BarPlot
 
-jpeg(file = "MyResults_GSEA/GSEA_dotplot.jpeg", units = 'in', width = 15, height = 10, res = 300)
-    par(mar = c(2, 2, 2, 5)) 
-    dotplot(egs, x = "GeneRatio", color = "pvalue", showCategory = 20, font.size = 15)
+jpeg(file = "MyResults_GSEA/Dotplot.jpeg", units = 'in', width = 15, height = 10, 
+     res = 300)
+par(mar = c(2, 2, 2, 5)) 
+dotplot(egs, x = "GeneRatio", color = "p.adjust", showCategory = 20, 
+            font.size = 15)
 invisible(dev.off())
 
 ##Gene-concept network
 
-jpeg(file = "MyResults_GSEA/GSEA_gene_concept_net.jpeg", units = 'in', width = 15, height = 10, res = 300)
-    par(mar = c(2, 2, 2, 5)) 
-    cnetplot(egs_genename, categorySize="pvalue", foldChange=dat, font.size = 15, colorEdge = T)
+jpeg(file = "MyResults_GSEA/Gene_concept_net.jpeg", units = 'in', 
+     width = 15, height = 10, res = 300)
+par(mar = c(2, 2, 2, 5)) 
+cnetplot(egs, categorySize="p.adjust", font.size = 15, colorEdge = T)
 invisible(dev.off())
 
 ##Ridgeline plot
 
 jpeg(file = "MyResults_GSEA/GSEA_ridge.jpeg", units = 'in', width = 15, height = 10, res = 300)
-    par(mar = c(2, 2, 2, 5)) 
-    ridgeplot(egs, fill="pvalue")
-invisible(dev.off())
-
-##Heatplot
-
-jpeg(file = "MyResults_GSEA/GSEA_ridge.jpeg", units = 'in', width = 20, height = 10, res = 300)
-    heatplot(egs, foldChange=dat)
+par(mar = c(2, 2, 2, 5)) 
+ridgeplot(egs, fill="p.adjust", orderBy= 'NES', core_enrichment = T)
 invisible(dev.off())
 
 ##Upset plot (of the 20 first terms)
@@ -73,7 +79,7 @@ lista_20first <- list()
 for (i in 1:nrow(genes_20first)){
     lista_20first[[i]] <- unlist(strsplit(as.character(genes_20first[i,1]),split="/"))   
 }
-uniq_genes <- as.character(unique(names(dat)))
+uniq_genes <- as.character(unique(names(dat))) #es necesario?
 
 func_20first <- egs$Description[1:20]
 mat <- matrix(0L, nrow = length(uniq_genes), ncol = length(func_20first)) 
@@ -105,3 +111,5 @@ for (j in 1:5){
   ggsave(pl, file=filename, device = "jpeg", units= "in", height = 15, width = 20)
 }
 
+gseap <- gseaplot2(egs, geneSetID = 'todos', pvalue_table = T)
+ggsave(gseap, file='All gseaplots', device = "jpeg", units= "in", height = 15, width = 20)
